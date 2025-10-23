944e9c1268a138ab23edb7493759751e2246ecef
contactforestblue-code <contact.forest.blue@gmail.com>
2025-10-23 20:50:45 +1100

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,7 @@
 let snake;
-let rez = 20; // size of each grid cell
+let rez = 20;
 let food;
-let w;
-let h;
+let w, h;
 let gameOver = false;
 let score = 0;
 
@@ -15,6 +14,9 @@ function setup() {
   snake = new Snake();
   foodLocation();
   textAlign(CENTER, CENTER);
+
+  // Touch controls
+  setupMobileControls();
 }
 
 function foodLocation() {
@@ -31,7 +33,7 @@ function draw() {
     textSize(24);
     text("Game Over!", width / 2, height / 2 - 20);
     textSize(16);
-    text("Press ENTER to restart", width / 2, height / 2 + 10);
+    text("Press ENTER or tap ⟳ to restart", width / 2, height / 2 + 10);
     text(`Score: ${score}`, width / 2, height / 2 + 40);
     return;
   }
@@ -54,17 +56,11 @@ function draw() {
 }
 
 function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
-    snake.setDir(-1, 0);
-  } else if (keyCode === RIGHT_ARROW) {
-    snake.setDir(1, 0);
-  } else if (keyCode === DOWN_ARROW) {
-    snake.setDir(0, 1);
-  } else if (keyCode === UP_ARROW) {
-    snake.setDir(0, -1);
-  } else if (keyCode === ENTER && gameOver) {
-    resetGame();
-  }
+  if (keyCode === LEFT_ARROW) snake.setDir(-1, 0);
+  else if (keyCode === RIGHT_ARROW) snake.setDir(1, 0);
+  else if (keyCode === DOWN_ARROW) snake.setDir(0, 1);
+  else if (keyCode === UP_ARROW) snake.setDir(0, -1);
+  else if (keyCode === ENTER && gameOver) resetGame();
 }
 
 function resetGame() {
@@ -83,7 +79,6 @@ class Snake {
   }
 
   setDir(x, y) {
-    // Prevent reversing direction directly
     if (this.body.length > 1) {
       let head = this.body[this.body.length - 1];
       let neck = this.body[this.body.length - 2];
@@ -97,21 +92,17 @@ class Snake {
     if (this.xdir === 0 && this.ydir === 0) return;
 
     let head = this.body[this.body.length - 1].copy();
-
     head.x += this.xdir;
     head.y += this.ydir;
 
-    // Wrap-around logic
+    // Wrap-around
     if (head.x >= w) head.x = 0;
     if (head.x < 0) head.x = w - 1;
     if (head.y >= h) head.y = 0;
     if (head.y < 0) head.y = h - 1;
 
     this.body.push(head);
-
-    if (this.body.length > this.len + 1) {
-      this.body.shift();
-    }
+    if (this.body.length > this.len + 1) this.body.shift();
 
     this.endGame();
   }
@@ -138,9 +129,36 @@ class Snake {
 
   show() {
     fill(0, 255, 0);
-    for (let i = 0; i < this.body.length; i++) {
-      let part = this.body[i];
+    for (let part of this.body) {
       rect(part.x * rez, part.y * rez, rez, rez);
     }
   }
+}
+
+function setupMobileControls() {
+  const dirs = {
+    up: [0, -1],
+    down: [0, 1],
+    left: [-1, 0],
+    right: [1, 0]
+  };
+
+  for (let id in dirs) {
+    const btn = document.getElementById(id);
+    if (btn) {
+      btn.addEventListener("touchstart", e => {
+        e.preventDefault();
+        snake.setDir(...dirs[id]);
+      });
+    }
+  }
+
+  const restartBtn = document.getElementById("restart");
+  restartBtn.addEventListener("touchstart", e => {
+    e.preventDefault();
+    if (gameOver) {
+      loop();
+      resetGame();
+    }
+  });
 }
